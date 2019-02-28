f982b210c193f293a777a003b25046265d1aae21
Nathaniel Tucker <35520072+natesqrd@users.noreply.github.com>
2019-02-28 11:37:36 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -160,3 +160,55 @@ main{
         int h = 13.4;
         int g = h * int;
 }
+
+
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+using namespace std;
+int mainChamber()
+{
+    string resp;
+    int room = 0;
+    cout << "You have entered the main chamber\n";
+    cout << "Choose a path\n";
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "Enter path " << i + 1 << "? yes/no\n";
+        cin >> resp;
+        if(resp == "yes")
+        {
+            room = i;
+            break;
+        }
+        if(resp != "yes" and resp != "no")
+        {
+            cout << "Sorry I do not understand that response\nPlease continue\n";
+            i--;
+        }
+        if(i + 1 == 5)
+            i = -1;
+    }
+    return room;
+}
+int main()
+{
+    string resp;
+    int room;
+    cout << "Welcome to the maze\nWould you like to enter? yes/no\n";
+    cin >> resp;
+    if(resp == "yes")
+    {
+        room = mainChamber();
+    }
+    else if(resp == "no")
+    {
+        cout << "You've made it out this time\n";
+    }
+    else{
+        cout << "Sorry I do not unserstand this response\n";
+    }
+    return 0;
+}
